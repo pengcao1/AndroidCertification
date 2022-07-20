76179b5e3a590dd85d64f6b25bb7c98f06cae96f
Peng.Cao <a3825aa@gmail.com>
2022-07-20 16:48:39 +0800

    升级Deprecated代码

--- app/src/main/java/com/cp/androidcertification/ui/main/MainFragment.kt
+++ app/src/main/java/com/cp/androidcertification/ui/main/MainFragment.kt
@@ -21,8 +21,8 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
